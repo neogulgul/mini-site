e2ca4280c2ecf874ea6875f93bb1515b10283764
Neo Gullberg <neogullberg04@gmail.com>
2021-12-01 17:26:58 +0100

    done some stuff

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+// hamburger menu
 const burgerBtn = document.querySelector(".burger-btn");
-const navMenu = document.querySelector(".nav-menu")
+const navMenu = document.querySelector(".nav-menu");
 
 burgerBtn.addEventListener("click", hamburgerMenu)
 
 function hamburgerMenu() {
     burgerBtn.classList.toggle("active");
     navMenu.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
 }
+
+// upvotes and downvotes
+const upVote = document.querySelector(".upvote");
+const downVote = document.querySelector(".downvote");
+
+upVote.addEventListener("click", () => upVote.classList.toggle("active"));
+downVote.addEventListener("click", () => downVote.classList.toggle("active"));
