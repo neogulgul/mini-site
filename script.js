94714b5e41f5397eca2abd005b98e9804cbad0b7
Neo Gullberg <neogullberg04@gmail.com>
2021-12-02 16:35:43 +0100

    stuff

--- script.js
+++ script.js
@@ -7,35 +7,5 @@ burgerBtn.addEventListener("click", hamburgerMenu)
 function hamburgerMenu() {
     burgerBtn.classList.toggle("active");
     navMenu.classList.toggle("active");
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.toggle("no-scroll")
 }
-
-// upvotes and downvotes
-const upvoteHollow = document.querySelector(".upvote-hollow");
-const upvoteFull = document.querySelector(".upvote-full");
-const downvoteHollow = document.querySelector(".downvote-hollow");
-const downvoteFull = document.querySelector(".downvote-full");
-
-upvoteHollow.addEventListener("click", () => {
-    upvoteHollow.classList.add("inactive");
-    upvoteFull.classList.remove("inactive");
-    downvoteHollow.classList.remove("inactive");
-    downvoteFull.classList.add("inactive");
-})
-
-upvoteFull.addEventListener("click", () => {
-    upvoteHollow.classList.remove("inactive");
-    upvoteFull.classList.add("inactive");
-})
-
-downvoteHollow.addEventListener("click", () => {
-    upvoteHollow.classList.remove("inactive");
-    upvoteFull.classList.add("inactive");
-    downvoteHollow.classList.add("inactive");
-    downvoteFull.classList.remove("inactive");
-})
-
-downvoteFull.addEventListener("click", () => {
-    downvoteHollow.classList.remove("inactive");
-    downvoteFull.classList.add("inactive");
-})
